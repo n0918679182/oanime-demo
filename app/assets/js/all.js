80a2a3a9cad614b33dd18e0932cf90b9b5834b1d
wayne <n0918679182@gmail.com>
2022-07-06 19:56:47 +0800

    ver1.0

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,52 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// header 點擊下拉搜尋
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.search-form');
+searchBtn.addEventListener('click', ()=>{
+    searchBtn.classList.toggle('bx-x');
+    searchForm.classList.toggle('active');
+    userMenu.classList.remove('active');
 });
+
+
+// header 點擊下拉選單 或 點擊登入
+let userBtn = document.querySelector('#user-btn');
+let userMenu = document.querySelector('.user-menu');
+let havelogin = "true";
+let loginClose = document.querySelector('#close-btn');
+let logout = document.querySelector('#logout');
+let login = document.querySelector('.login-btn');
+
+login.addEventListener('click',()=>{
+    havelogin = "true";
+});
+
+userBtn.addEventListener('click', ()=>{
+    alert(havelogin)
+    searchBtn.classList.remove('bx-x');
+    searchForm.classList.remove('active');
+    if( havelogin == "true" ){
+        userMenu.classList.add('active');
+    }else{
+        document.getElementById("user-menu").style.display="none";
+        document.getElementById("login-container").style.top=0;
+    }
+});
+
+logout.addEventListener('click',()=>{
+    havelogin = "false";
+});
+
+loginClose.addEventListener('click',()=>{
+    document.getElementById("login-container").style.top="-120%";
+});
+
+
+
+
+// 滑動卷軸關閉視窗
+window.onscroll = () =>{
+  userMenu.classList.remove('active');
+  searchForm.classList.remove('active');
+  searchBtn.classList.remove('active');
+  // document.getElementById("login-container").style.top="-120%";
+}
